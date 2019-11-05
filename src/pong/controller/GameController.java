6f0567ca375198216864961d12c891e9eacd092b
Kris Peklaric <peklarick@gmail.com>
2019-11-05 05:58:30 +0100

    refactored grid, init setup finished

--- src/pong/controller/GameController.java
+++ src/pong/controller/GameController.java
@@ -1,34 +1,27 @@
 package pong.controller;
 
-import com.sun.javafx.geom.BaseBounds;
-import com.sun.javafx.geom.transform.BaseTransform;
-import com.sun.javafx.jmx.MXNodeAlgorithm;
-import com.sun.javafx.jmx.MXNodeAlgorithmContext;
-import com.sun.javafx.sg.prism.NGNode;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
-import javafx.geometry.VPos;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 
-import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
-    private static int[][] gridFieldset = new int[32][32];
-
+    private static final int SIZE = 23;
+//    private static final int[][] DIRECTIONS = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,0],[0,1],[1,-1],[1,0],[1,1]];
+    private static final byte BALL_DIRECTION = -1;
+    private static int[][] gridFields = new int[SIZE][SIZE];
     @FXML
     public GridPane gameGrid;
     @FXML
@@ -50,68 +43,82 @@ public class GameController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         setBoardFields();
-        boardInit();
+        boardRender();
     }
 
     private void setBoardFields() {
-        for (int i = 0; i < 32; i++) {
-            for (int j = 0; j < 32; j++) {
-                gridFieldset[i][j] = 0;
+        //Filling Start values
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                gridFields[i][j] = 0;
 
-                if (i > 17 ) {
-                    gridFieldset[i][j] = 0;
+                //Obstacles
+                if (i == 8 && (j > 10 && j < 16)) {
+                    gridFields[i][j] = 1;
+                } else if (i == 6 && (j > 3 && j < 9)) {
+                    gridFields[i][j] = 1;
+                } else {
+                    gridFields[i][j] = 0;
                 }
-                else    {
-                    gridFieldset[i][j] = 1;
-                }
-
-                if ( i == 30 && (j > 20 || j < 24)) {
-                    gridFieldset[i][j] = 92;
+                //Player
+                if (i == 22 && (j > 12 && j < 16)) {
+                    gridFields[i][j] = 92;
                 }
             }
         }
 
-        gridFieldset[24][16] = 99;
+        gridFields[18][16] = 99;
 
-
-    }
-
-    private void boardInit() {
-        final int numCols = 32 ;
-        final int numRows = 32 ;
-        for (int i = 0; i < numCols; i++) {
+        //Adding GRID Cols and Rows
+        for (int i = 0; i < SIZE; i++) {
             ColumnConstraints colConst = new ColumnConstraints();
             colConst.setPercentWidth(100.0);
             colConst.setHalignment(HPos.CENTER);
             gameGrid.getColumnConstraints().add(colConst);
         }
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 0; i < SIZE; i++) {
             RowConstraints rowConst = new RowConstraints();
-            rowConst.setPercentHeight(100.0 );
+            rowConst.setPercentHeight(100.0);
             gameGrid.getRowConstraints().add(rowConst);
         }
-        for (int i = 0; i < 32; i++) {
-            for (int j = 0; j < 32; j++) {
+    }
 
-//                Ovdje treba ici provjera iz fieldseta koji je i ovisno o tome iscrtati
-//                Zapreke za sada staviti rucno
-                if ( j == 27 && i == 16) {
-                    ball.setCenterX(50.0f);
-                    ball.setCenterY(50.0f);
-                    ball.setRadius(12.0f);
-                    ball.setFill(Color.GRAY);
-                    gameGrid.add(ball,i,j);
-                }
-                if (j == 30 && (i > 14 && i < 19)) {
-                    Rectangle temp = new Rectangle();
-                    temp.setWidth(45);
-                    temp.setHeight(20);
-                    temp.setFill(Color.GRAY);
-                    temp.setStroke(Color.DARKSLATEGREY);
-                    gameGrid.add(temp,i,j);
+    public void boardRender() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                switch (gridFields[i][j]) {
+
+                    case 1:
+                        Rectangle temp = new Rectangle();
+                        temp.setWidth(55);
+                        temp.setHeight(25);
+                        temp.setFill(Color.YELLOW);
+                        temp.setStroke(Color.YELLOW);
+                        gameGrid.add(temp, j, i);
+                    case 92:
+                        Rectangle player = new Rectangle();
+                        player.setWidth(65);
+                        player.setHeight(30);
+                        player.setFill(Color.DARKGRAY);
+                        player.setStroke(Color.DARKSLATEGREY);
+                        gameGrid.add(player, j, i);
+                        break;
+                    case 99:
+                        ball.setCenterX(50.0f);
+                        ball.setCenterY(50.0f);
+                        ball.setRadius(12.0f);
+                        ball.setFill(Color.GRAY);
+                        gameGrid.add(ball, j, i);
+                        break;
+                    default:
+                        continue;
                 }
             }
         }
+    }
+
+    private void nextTick() {
+
     }
 
     @FXML
